257e1919c0c38d3488ab6897d27efb960260dc12
SergKazakov <sergeykazakoff@yandex.ru>
2014-01-31 13:19:07 +0400

    added grunt.js to this project to automate css minifying replaced formstyler plugin for checkbox with icheck plugin added PIE.htc for IE 7-8 There are some bugs with PIE.htc

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+     cssmin: { 
+            add_banner: {
+                options: {
+                    banner: '/* Minified CSS */'  
+                },
+                files: {
+                    'css/main.min.css' : ['css/reset.css', 'css/ie.css' ,'css/main.css' ]  
+                } 
+            }
+        },
+    watch: { 
+            css: {
+                files: ['css/*.css'],
+                tasks: ['cssmin'] ,
+            options: {
+                livereload: true,
+            }
+          }
+        },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['cssmin', 'watch']);
+};
